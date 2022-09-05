d1877f23aabed40401100f28e4fd89bd60fef301
Asus <beketaisha2006@gmail.com>
2022-09-05 20:04:55 +0500

    second commit

--- main.py
+++ main.py
@@ -79,3 +79,17 @@ def get_user_text(message):
                 bot.send_message(message.chat.id, "Ищем книгу...")
                 bot.send_document(message.chat.id, file_book)
                 file_book.close()
+
+
+@server.route(f"/{BOT_TOKEN}", methods=["POST"])
+def redirect_message():
+    json_string = request.get_data().decode("utf-8")
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
