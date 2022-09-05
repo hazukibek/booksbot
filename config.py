98ceb5b29a2acd62e35251a62f969a26b0550b1c
Asus <beketaisha2006@gmail.com>
2022-09-05 20:02:54 +0500

    first commit

--- config.py
+++ config.py
@@ -1,8 +1,2 @@
-import os
-import telebot
-import logging
-import openpyxl
-from config import *
-from flask import Flask, request
-from telebot import types
-
+BOT_TOKEN = "5401286450:AAFsTKIQIlE5r7ySlRf1MGju6z8UdonZVy4"
+APP_URL = "https://juz-companion.herokuapp.com/" + BOT_TOKEN
